c8530f9dbd0278722132f3968f40a09d1b788fff
Alexei <alexeitara5ov@yandex.ru>
2020-03-13 12:37:07 +0300

    [ex1-12] Uskoreniyeraboty s kontentom: html zagotovka s kontentom inner.html

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
-<? //global $USER;
-   //$USER->Authorize(1); ?>
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("");?><? //global $USER;
+   //$USER->Authorize(1); ?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
